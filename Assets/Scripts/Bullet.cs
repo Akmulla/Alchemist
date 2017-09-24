6d6dc2293bfff46628b7513a95d9d33a4b1f7262
Akmulla <d4n73@yandex.ru>
2017-09-24 18:06:43 +0600

    enemy chngs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public PoolRef pool_ref;
     public string target;
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.CompareTag("Boundary"))
+        {
+            //other.gameObject.GetComponent<Character>().GetHit(1);
+            pool_ref.GetPool().Deactivate(gameObject);
+            //Destroy(gameObject);
+        }
+
         if (other.gameObject.CompareTag(target))
         {
             other.gameObject.GetComponent<Character>().GetHit(1);
-            Destroy(gameObject);
+            pool_ref.GetPool().Deactivate(gameObject);
+            //Destroy(gameObject);
         }
 
     }
